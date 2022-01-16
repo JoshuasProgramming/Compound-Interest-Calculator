d82a438fa0e53b086b7938e5fac01d1182ee8259
JoshuasProgramming <90353177+JoshuasProgramming@users.noreply.github.com>
2022-01-16 16:58:48 +0000

    Added separate pages with unique js files
    index 1,2,3 are pages that have different compound intervals attached to them. 
    script 1,2,3 are the same as above.

---
+++ script2.js
@@ -0,0 +1,21 @@
+/**
+ *Compound Interest Calculator for monthly JavaScript File
+ *author(Joshua Thomas)
+ *date(16.01.2022)
+ *version:1.0
+*/
+
+const principle = document.getElementById('principle');
+const interest = document.getElementById('interest');
+const duration = document.getElementById('duration');
+const output = document.getElementById('output');
+
+const btn_ = document.getElementById('btn_').addEventListener("click", (e)=>{
+    let percentage = (interest.value / 100); //8 => 0.08 => 8%
+    let n = 12;
+    
+    let A1 = principle.value*(1 + (percentage/n)) ** (n * duration.value);
+    let A2 = A1.toLocaleString("en-US");
+    
+    output.value = "£" + A2;
+});
