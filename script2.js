f554317e09915a62633344d9454e82e72666d8f5
JoshuasProgramming <90353177+JoshuasProgramming@users.noreply.github.com>
2022-01-16 21:20:20 +0000

    Add files via upload
    added JavaScript for daily, monthly and yearly compounding

--- script2.js
+++ script2.js
@@ -10,12 +10,28 @@ const interest = document.getElementById('interest');
 const duration = document.getElementById('duration');
 const output = document.getElementById('output');
 
+//grab monthly id for monthly contributions
+const monthly = document.getElementById('monthly');
+
 const btn_ = document.getElementById('btn_').addEventListener("click", (e)=>{
+
+    if((monthly.value == "") || (monthly.value == "0")){
+        monthly.value = 0;
+    }
+
     let percentage = (interest.value / 100); //8 => 0.08 => 8%
-    let n = 12;
+    let n = 12; //12 => compounds 12 time per year (Monthly compound interval)
     
-    let A1 = principle.value*(1 + (percentage/n)) ** (n * duration.value);
-    let A2 = A1.toLocaleString("en-US");
+    let A1 = (principle.value*(1 + (percentage/n)) ** (n * duration.value));
+    let pmt_v = ((monthly.value*(1 + (percentage/n)) ** (n * duration.value)) - monthly.value) / (percentage/n);
+
+    //alert("A1: " + A1 + "PMT: " + pmt);
+
+    let A2 = (A1 + pmt_v);
+
+    //alert(A2);
     
-    output.value = "£" + A2;
+    let A3 = A2.toLocaleString("en-US");
+    
+    output.value = "£" + A3;
 });
--- script3.js
+++ script3.js
@@ -10,12 +10,28 @@ const interest = document.getElementById('interest');
 const duration = document.getElementById('duration');
 const output = document.getElementById('output');
 
+//grab monthly id for monthly contributions
+const daily = document.getElementById('daily');
+
 const btn_ = document.getElementById('btn_').addEventListener("click", (e)=>{
+
+    if((daily.value == "") || (daily.value == "0")){
+        daily.value = 0;
+    }
+
     let percentage = (interest.value / 100); //8 => 0.08 => 8%
-    let n = 365;
+    let n = 365; //365 => compounds 365 time per year (Daily compound interval)
     
-    let A1 = principle.value*(1 + (percentage/n)) ** (n * duration.value);
-    let A2 = A1.toLocaleString("en-US");
+    let A1 = (principle.value*(1 + (percentage/n)) ** (n * duration.value));
+    let pmt_v = ((daily.value*(1 + (percentage/n)) ** (n * duration.value)) - daily.value) / (percentage/n);
+
+    //alert("A1: " + A1 + "PMT: " + pmt);
+
+    let A2 = (A1 + pmt_v);
+
+    //alert(A2);
     
-    output.value = "£" + A2;
+    let A3 = A2.toLocaleString("en-US");
+    
+    output.value = "£" + A3;
 });
